037aa17f71645b63ce1aec72d22a1a31b36e013e
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2024-12-08 21:28:56 +0530

    Started to build color guessing game Just created the sqares and created the styles.css and script.js files and do a little code

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var colors = [
+  "rgb(255, 0, 0)",
+  "rgb(255, 110, 0)",
+  "rgb(255, 0, 110)",
+  "rgb(255, 50, 50)",
+  "rgb(255, 88, 20)",
+  "rgb(255, 90, 30)",
+];
+
+var sqares = document.querySelectorAll(".square");
+
+for (let i = 0; i < sqares.length; i++) {
+  squares[i].style.backgroundColor = colors[i];
+}
